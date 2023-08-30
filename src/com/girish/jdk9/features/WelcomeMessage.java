f42cce864b97af91558ca288e27fde9f247ba671
GirishKumar3763 <65503614+GirishKumar3763@users.noreply.github.com>
2023-08-30 09:08:54 +0530

    first message

---
+++ src/com/girish/jdk9/features/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package com.girish.jdk9.features;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Welcome Girish");
+	}
+
+}
